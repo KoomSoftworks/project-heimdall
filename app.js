f65d84bca18ed53f4fb0e668de79b411e05c65e0
Lenny Esquivel <46729@hexaware.com>
2020-10-27 16:46:28 -0600

    working on execution

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
+const bcrypt = require('bcryptjs');
 
 const firebase = require("firebase/app");
 require("firebase/auth");
@@ -29,8 +30,41 @@ let firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+let db = firebase.firestore();
+
 app.get('/api', (req, res) => {
     res.send('<h1>Working!</h1>');
+});
+
+app.post('/api/user/register', (req, res, next) => {
+    const user = { 
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password, 42)
+    };
+    console.log(user);
+    if (!user.name || !user.email || !user.username || !user.password) {
+        response = {
+            error: true,
+            code: 400,
+            message: 'Missing data'
+        };
+        res.status(400).send(response);
+    } else {
+        let userRef = db.collection('users').doc(user.username);
+        if (userRef) {
+            console.log(userRef);
+        }
+    }
+});
+
+app.post('/api/user/login', (req, res, next) => {
+
+});
+
+app.post('/api/execution', (req, res, next) => {
+
 });
 
 app.get('*', (req, res) => {
